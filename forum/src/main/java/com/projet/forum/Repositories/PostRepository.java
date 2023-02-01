ebd682dff6774597f13fae1082a9daa0ea952ee4
RitchieHollis <wiktorchabowski02@gmail.com>
2023-02-01 12:20:12 +0100

    crudRepository added

--- forum/src/main/java/com/projet/forum/Repositories/PostRepository.java
+++ forum/src/main/java/com/projet/forum/Repositories/PostRepository.java
@@ -1,11 +1,10 @@
 package com.projet.forum.Repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projet.forum.Entities.PostEntity;
 
 @Repository
-public interface PostRepository extends JpaRepository<PostEntity,Long>{
+public interface PostRepository extends CrudRepository<PostEntity,Long>{
     
 }
--- forum/src/main/java/com/projet/forum/Repositories/UserInfoRepository.java
+++ forum/src/main/java/com/projet/forum/Repositories/UserInfoRepository.java
@@ -1,11 +1,10 @@
 package com.projet.forum.Repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projet.forum.Entities.UserInfoEntity;
 
 @Repository
-public interface UserInfoRepository extends JpaRepository<UserInfoEntity,Long>{
+public interface UserInfoRepository extends CrudRepository<UserInfoEntity,Long>{
     
 }
