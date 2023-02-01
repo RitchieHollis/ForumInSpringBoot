f390598d12151af90a5983ae6b51c5302df6f70d
RitchieHollis <wiktorchabowski02@gmail.com>
2023-02-01 12:18:23 +0100

    crud repository added

--- forum/src/main/java/com/projet/forum/Repositories/ChannelRepository.java
+++ forum/src/main/java/com/projet/forum/Repositories/ChannelRepository.java
@@ -1,11 +1,10 @@
 package com.projet.forum.Repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projet.forum.Entities.ChannelEntity;
 
 @Repository
-public interface ChannelRepository extends JpaRepository<ChannelEntity,Long>{
+public interface ChannelRepository extends CrudRepository<ChannelEntity,Long>{
     
 }
--- forum/src/main/java/com/projet/forum/Repositories/MessageRepository.java
+++ forum/src/main/java/com/projet/forum/Repositories/MessageRepository.java
@@ -1,11 +1,10 @@
 package com.projet.forum.Repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projet.forum.Entities.MessageEntity;
 
 @Repository
-public interface MessageRepository extends JpaRepository<MessageEntity,Long>{
+public interface MessageRepository extends CrudRepository<MessageEntity,Long>{
     
 }
